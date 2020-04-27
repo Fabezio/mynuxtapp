c068c4c95f6e62d0a73b485bb886a9dda0d2f60f
fabezio <fabezio@outlook.fr>
2020-04-27 20:51:04 +0200

    suppression fichiers superflus, creation plugin hierarchique

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#0ff' },
   /*
    ** Global CSS
    */
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/main', '~/plugins/logos'],
   /*
    ** Nuxt.js dev-modules
    */
